45bb89e6d861949a3a66a4986185e515be2b6f7c
iSAD <bradx2713@gmail.com>
2024-07-27 06:06:29 +0200

    Initial Commit

---
+++ Card_Game_R15/Card_Game_R15App.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Game_R15App.swift
+//  Card_Game_R15
+//
+//  Created by Tafadzwa Alexander on 2024/07/27.
+//
+
+import SwiftUI
+
+@main
+struct Card_Game_R15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card_Game_R15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Card_Game_R15
+//
+//  Created by Tafadzwa Alexander on 2024/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
